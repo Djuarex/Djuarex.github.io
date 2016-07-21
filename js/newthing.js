9eb5c6c999c9ff2c3c0f4868bd008709a9052c24
Djuarex <tigerbear26@gmail.com>
2016-07-21 10:39:42 -0700

    Finshed kind of

--- js/newthing.js
+++ js/newthing.js
@@ -1,3 +1,11 @@
-$("img1").click(function(){
-	$(this).attr("src", "images/Husky.jpg");
+
+ $(document).ready(function(){
+ 	$(".img1").click(function(){
+ 		$(this).attr("src", "images/white_dog.jpg");
+
 	})
+
+ })
+
+
+
